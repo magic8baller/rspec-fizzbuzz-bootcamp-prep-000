2d81c42622169d8d3e99b965f889a4862bb3d671
Leslie Turis <lturis@gmail.com>
2018-08-30 19:59:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 15 === 0
-    puts 'FizzBuzz'
+    'FizzBuzz'
   elsif int % 3 === 0
-    puts 'Fizz'
+    'Fizz'
   elsif int % 5 === 0
-    puts 'Buzz'
+    'Buzz'
   end
 end
